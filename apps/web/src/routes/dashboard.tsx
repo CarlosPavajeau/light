63341dab4fa8d090008467fac4048b9f5ad41093
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-04-26 12:28:12 -0500

    chore: drop unnecessary code

--- apps/web/src/routes/dashboard.tsx
+++ apps/web/src/routes/dashboard.tsx
@@ -1,13 +1,5 @@
 import { Button } from "@light/ui/components/button"
-import {
-  Dialog,
-  DialogTrigger,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-} from "@light/ui/components/dialog"
-import { useQuery } from "@tanstack/react-query"
+import { useMutation } from "@tanstack/react-query"
 import { createFileRoute, redirect } from "@tanstack/react-router"
 
 import { getUser } from "@/functions/get-user"
@@ -32,26 +24,20 @@ function RouteComponent() {
   const { session } = Route.useRouteContext()
 
   const trpc = useTRPC()
-  const privateData = useQuery(trpc.privateData.queryOptions())
+
+  const { mutate } = useMutation(
+    trpc.projects.create.mutationOptions({
+      onSuccess: () => {
+        console.log("Project created")
+      },
+    })
+  )
 
   return (
     <div>
       <h1>Dashboard</h1>
       <p>Welcome {session?.user.name}</p>
-      <p>API: {privateData.data?.message}</p>
-
-      <Dialog>
-        <DialogTrigger render={<Button />}>Open</DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Are you absolutely sure?</DialogTitle>
-            <DialogDescription>
-              This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers.
-            </DialogDescription>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+      <Button onClick={() => mutate({ name: "Prueba" })}>Create Project</Button>
     </div>
   )
 }
