e8c197515afbc8f2c6100e92ccd963396f288c51
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-04-29 08:11:02 -0500

    feat: add s3 package

--- packages/env/src/server.ts
+++ packages/env/src/server.ts
@@ -8,6 +8,8 @@ export const env = createEnv({
     BETTER_AUTH_SECRET: z.string().min(32),
     BETTER_AUTH_URL: z.url(),
     CORS_ORIGIN: z.url(),
+    AWS_REGION: z.string(),
+    AWS_IDENTITY_POOL_ID: z.string(),
     NODE_ENV: z
       .enum(["development", "production", "test"])
       .default("development"),
