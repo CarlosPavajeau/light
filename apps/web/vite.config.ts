5a3ebe2d0d0ac73c93f71ad09e61766c0d685fec
CarlosPavajeau <cpavajea@emeal.nttdata.com>
2026-05-01 16:47:38 -0500

    chore: revert cloudflare configuration

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,7 @@
-import { cloudflare } from "@cloudflare/vite-plugin"
 import tailwindcss from "@tailwindcss/vite"
 import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
+import { nitro } from "nitro/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
@@ -11,10 +11,5 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [
-    tailwindcss(),
-    cloudflare({ viteEnvironment: { name: "ssr" } }),
-    tanstackStart(),
-    viteReact(),
-  ],
+  plugins: [tailwindcss(), tanstackStart(), nitro(), viteReact()],
 })
